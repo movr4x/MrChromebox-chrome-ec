6b50714e3b00a7f6f3b592be76ff02bf63796b37
Matt DeVillier <matt.devillier@gmail.com>
2024-07-20 16:50:56 -0500

    util/build.mk: don't build iteflash
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- util/build.mk
+++ util/build.mk
@@ -7,7 +7,7 @@
 #
 
 host-util-bin=ectool lbplay stm32mon ec_sb_firmware_update lbcc \
-	ec_parse_panicinfo cbi-util iteflash
+	ec_parse_panicinfo cbi-util
 build-util-bin=ec_uartd
 build-util-art+=util/export_taskinfo.so
 ifeq ($(CHIP),npcx)
